b1de63cee7a4af9ee9dc257e42967011fc666428
abdservus <abdccassiano@hotmail.com>
2021-12-15 18:41:13 -0600

    Curso React: Se agrego un json para recorrer las peliculas

--- src/App.js
+++ src/App.js
@@ -5,20 +5,39 @@ import PageWrapper from './PageWrapper';
 
 function App() {
 
+  let peliculas = [
+    {
+      titulo: "into the wild (2014)",
+      calificacion: "7.8",
+      director: "Joss Whedon",
+      actores: "Robert Downey Jr., Chris Evans, Chris Hemsworth",
+      duracion: "2h 21min",
+      img: "images/uploads/mv1.jpg",
+      descripcion: "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
+    },
+    {
+      titulo: "oblivion (2012)",
+      calificacion: "8.1",
+      director: "Joss Whedon",
+      actores: "Robert Downey Jr., Chris Evans, Chris Hemsworth",
+      duracion: "2h 21min",
+      img: "images/uploads/mv2.jpg",
+      descripcion: "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
+    }
+  ]
+
   return (
     <PageWrapper>
 
-      <Pelicula titulo="into the wild (2014)" calificacion="7.8" 
-        director="Joss Whedon" actores="Robert Downey Jr., Chris Evans, Chris Hemsworth" 
-        fecha="1 May 2015" duracion="2h 21min" img="images/uploads/mv1.jpg">
-          Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity...
+      {peliculas.map(pelicula => {
+        return (
+        <Pelicula titulo={pelicula.titulo} calificacion={pelicula.calificacion} 
+        director={pelicula.director} actores={pelicula.actores} fecha={pelicula.fecha} 
+        duracion={pelicula.duracion} img={pelicula.img}>
+          {pelicula.descripcion}
         </Pelicula>
-
-        <Pelicula titulo="oblivion (2012)" calificacion="8.1" 
-        director="Joss Whedon" actores="Robert Downey Jr., Chris Evans, Chris Hemsworth" 
-        fecha="1 May 2015" duracion="2h 21min" img="images/uploads/mv2.jpg">
-          Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity...
-        </Pelicula>
+        )
+      })}
 
     </PageWrapper>
   );
